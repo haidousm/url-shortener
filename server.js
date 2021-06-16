b4a004e33b37dfbbb785390c342098764adc4332
haidousm <haidous.m@gmail.com>
2021-06-16 21:22:49 +0300

    setup mongodb

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv").config();
 
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+
+const app = express();
 app.use(express.static("public"));
+
+const mongoUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.x4n3c.mongodb.net/url_shortener?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.post("/shorten", (req, res) => {});
 
 app.listen(process.env.PORT || 5000);
