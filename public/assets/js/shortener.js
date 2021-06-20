de2b778bc3726b06e0e56caa95c95d2f3aca778a
haidousm <haidous.m@gmail.com>
2021-06-20 16:27:41 +0300

    add clientside validation

--- public/assets/js/shortener.js
+++ public/assets/js/shortener.js
@@ -1,3 +1,45 @@
+$(".url-input").on("keyup", function (e) {
+    if (e.key === "Enter" || e.keyCode === 13) {
+        $(".shorten-btn").click();
+    }
+});
+
+$(".shorten-btn").on("click", function (e) {
+    let full = $(".url-input").val();
+
+    if (!validURL(full)) {
+        $(".terminal-container").effect("shake");
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/shorten",
+
+            data: JSON.stringify({ url: full }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (res) {
+                $(".url-input").val(window.location.origin + "/" + res.short);
+                $(".url-input").attr("readonly", true);
+                $(".url-input").addClass("is-link");
+                $(".url-input").on("click", function () {
+                    location.href = $(".url-input").val();
+                });
+
+                $("label[for='url-input']").css({ width: "85%" });
+                $(".shorten-btn").html("Copy");
+                $(".shorten-btn").on("click", function () {
+                    copyToClipboard($(".url-input").val());
+                    location.href = location.href;
+                });
+            },
+            error: function (err) {
+                console.error(err);
+            },
+        });
+    }
+    $(e.target).blur();
+});
+
 const copyToClipboard = (str) => {
     const el = document.createElement("textarea");
     el.value = str;
@@ -7,39 +49,15 @@ const copyToClipboard = (str) => {
     document.body.removeChild(el);
 };
 
-$(".url-input").on("keyup", function (e) {
-    if (e.key === "Enter" || e.keyCode === 13) {
-        jQuery(".shorten-btn").click();
-    }
-});
-
-jQuery(".shorten-btn").on("click", function () {
-    let full = jQuery(".url-input").val();
-
-    jQuery.ajax({
-        type: "POST",
-        url: "/shorten",
-
-        data: JSON.stringify({ url: full }),
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (res) {
-            jQuery(".url-input").val(window.location.origin + "/" + res.short);
-            jQuery(".url-input").attr("readonly", true);
-            jQuery(".url-input").addClass("is-link");
-            jQuery(".url-input").on("click", function () {
-                location.href = jQuery(".url-input").val();
-            });
-
-            jQuery("label[for='url-input']").css({ width: "85%" });
-            jQuery(".shorten-btn").html("Copy");
-            jQuery(".shorten-btn").on("click", function () {
-                copyToClipboard(jQuery(".url-input").val());
-                location.href = location.href;
-            });
-        },
-        error: function (err) {
-            console.error(err);
-        },
-    });
-});
+const validURL = (str) => {
+    var pattern = new RegExp(
+        "^(https?:\\/\\/)?" +
+            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+            "((\\d{1,3}\\.){3}\\d{1,3}))" +
+            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+            "(\\?[;&a-z\\d%_.~+=-]*)?" +
+            "(\\#[-a-z\\d_]*)?$",
+        "i"
+    );
+    return !!pattern.test(str);
+};
