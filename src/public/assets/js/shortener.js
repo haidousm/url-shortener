8927349aea239e00972f0fe46a8f49fe7147dc41
haidousm <haidous.m@gmail.com>
2022-01-01 09:11:47 +0200

    rewrite most of script without jquery

--- src/public/assets/js/shortener.js
+++ src/public/assets/js/shortener.js
@@ -1,47 +1,50 @@
-$(".url-input").on("keyup", function (e) {
-    if (e.key === "Enter" || e.keyCode === 13) {
-        $(".shorten-btn").click();
-    }
-});
+const urlInput = document.querySelector(".url-input");
+const shortenBtn = document.querySelector(".shorten-btn");
+window.onload = () => {
+    urlInput.addEventListener("keyup", (e) => {
+        if (e.key === "Enter" || e.keyCode === 13) {
+            shortenBtn.click();
+        }
+    });
+    shortenBtn.addEventListener("click", async (e) => {
+        if (shortenBtn.innerHTML === "Copy") {
+            copyToClipboard(urlInput.value);
+            location.href = location.href;
+            return;
+        }
 
-$(".shorten-btn").on("click", function (e) {
-    let full = $(".url-input").val();
+        const fullUrl = urlInput.value;
+        if (!validURL(fullUrl)) {
+            $(".terminal-container").effect("shake");
+        } else {
+            const res = await fetch("/shorten", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    url: fullUrl,
+                }),
+            });
 
-    if (!validURL(full)) {
-        $(".terminal-container").effect("shake");
-    } else {
-        $.ajax({
-            type: "POST",
-            url: "/shorten",
+            if (res.status === 200) {
+                const data = await res.json();
+                urlInput.value = window.location.origin + "/" + data.short;
+                urlInput.attributes.readonly = true;
 
-            data: JSON.stringify({ url: full }),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (res) {
-                $(".url-input").val(window.location.origin + "/" + res.short);
-                $(".url-input").attr("readonly", true);
-                $(".url-input").css({
-                    "text-decoration": "underline",
-                    cursor: "pointer",
+                urlInput.style.textDecoration = "underline";
+                urlInput.style.cursor = "pointer";
+                urlInput.addEventListener("click", () => {
+                    location.href = urlInput.value;
                 });
-                $(".url-input").on("click", function () {
-                    location.href = $(".url-input").val();
-                });
 
-                $("label[for='url-input']").css({ width: "85%" });
-                $(".shorten-btn").html("Copy");
-                $(".shorten-btn").on("click", function () {
-                    copyToClipboard($(".url-input").val());
-                    location.href = location.href;
-                });
-            },
-            error: function (err) {
-                console.error(err);
-            },
-        });
-    }
-    $(e.target).blur();
-});
+                shortenBtn.innerText = "Copy";
+            }
+        }
+
+        e.target.blur();
+    });
+};
 
 const copyToClipboard = (str) => {
     const el = document.createElement("textarea");
@@ -64,3 +67,14 @@ const validURL = (str) => {
     );
     return !!pattern.test(str);
 };
+
+// const shake = (div, interval = 100, distance = 10, times = 4) => {
+//     $(div).css("position", "relative");
+//     for (var iter = 0; iter < times + 1; iter++) {
+//         $(div).animate(
+//             { left: iter % 2 == 0 ? distance : distance * -1 },
+//             interval
+//         );
+//     }
+//     $(div).animate({ left: 0 }, interval);
+// };
