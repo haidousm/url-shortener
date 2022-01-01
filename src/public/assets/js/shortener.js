ac03c01ffea06c57cf595bd100062515bd142177
haidousm <haidous.m@gmail.com>
2022-01-01 09:23:43 +0200

    impl shake anim in pure js

--- src/public/assets/js/shortener.js
+++ src/public/assets/js/shortener.js
@@ -1,5 +1,6 @@
 const urlInput = document.querySelector(".url-input");
 const shortenBtn = document.querySelector(".shorten-btn");
+const terminalContainer = document.querySelector(".terminal-container");
 window.onload = () => {
     urlInput.addEventListener("keyup", (e) => {
         if (e.key === "Enter" || e.keyCode === 13) {
@@ -15,7 +16,7 @@ window.onload = () => {
 
         const fullUrl = urlInput.value;
         if (!validURL(fullUrl)) {
-            $(".terminal-container").effect("shake");
+            shake(terminalContainer);
         } else {
             const res = await fetch("/shorten", {
                 method: "POST",
@@ -68,13 +69,22 @@ const validURL = (str) => {
     return !!pattern.test(str);
 };
 
-// const shake = (div, interval = 100, distance = 10, times = 4) => {
-//     $(div).css("position", "relative");
-//     for (var iter = 0; iter < times + 1; iter++) {
-//         $(div).animate(
-//             { left: iter % 2 == 0 ? distance : distance * -1 },
-//             interval
-//         );
-//     }
-//     $(div).animate({ left: 0 }, interval);
-// };
+const shake = (elem, interval = 100, distance = 100, times = 4) => {
+    elem.style.position = "relative";
+    for (var iter = 0; iter < times + 1; iter++) {
+        elem.animate(
+            {
+                transform: `translate(${
+                    iter % 2 == 0 ? distance : distance * -1
+                }px, 0)`,
+            },
+            interval
+        );
+    }
+    elem.animate(
+        {
+            transform: `translate(0, 0)`,
+        },
+        interval
+    );
+};
